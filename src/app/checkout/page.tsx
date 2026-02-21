24d1aaa90a208fb84ca7a9914b60146b62d94f15
Ubuntu <ubuntu@ip-172-31-32-139.eu-north-1.compute.internal>
2026-02-21 10:15:44 +0000

    Update to $39.99 plan

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -92,7 +92,7 @@ function CheckoutContent() {
 
     // Build Whop checkout URL
     // Format: https://whop.com/checkout/[plan_id]?email=[email]&d=[discount_code]
-    const WHOP_CHECKOUT_BASE = "https://whop.com/checkout/plan_UJlVQRBAuFu1t";
+    const WHOP_CHECKOUT_BASE = "https://whop.com/checkout/plan_gtlFi4zoHPy80";
     
     let whopUrl = `${WHOP_CHECKOUT_BASE}?email=${encodeURIComponent(email)}`;
     

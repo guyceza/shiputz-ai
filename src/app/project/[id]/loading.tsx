7e9b989e17b2f337c4cb74b58a2c9ec5eb545268
Ubuntu <ubuntu@ip-172-31-32-139.eu-north-1.compute.internal>
2026-02-26 15:57:48 +0000

    Add project-specific loading.tsx to show Business Website animation

---
+++ src/app/project/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingScreen from "@/components/LoadingScreen";
+
+export default function ProjectLoading() {
+  return <LoadingScreen text="טוען את הפרויקט..." tip="רגע ונהיה מוכנים" variant="project" />;
+}
